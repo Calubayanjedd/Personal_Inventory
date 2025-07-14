2df313f8af5ade1dc4f75110e368ff23fd64a140
Ruther Calubayan <rcalubayan@juntec.com.ph>
2025-07-14 16:41:03 +0800

    Added new design to the user controls

--- ucAddCustomers.Designer.cs
+++ ucAddCustomers.Designer.cs
@@ -29,27 +29,104 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            btnRegister = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(25, 26);
+            label1.Location = new Point(25, 20);
             label1.Name = "label1";
             label1.Size = new Size(132, 22);
             label1.TabIndex = 0;
             label1.Text = "Add Customers";
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(249, 134);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(396, 30);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(249, 218);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(396, 30);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(249, 301);
+            textBox3.Margin = new Padding(3, 2, 3, 2);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(396, 30);
+            textBox3.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(113, 142);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 22);
+            label2.TabIndex = 4;
+            label2.Text = "Fullname:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(120, 221);
+            label3.Name = "label3";
+            label3.Size = new Size(82, 22);
+            label3.TabIndex = 5;
+            label3.Text = "Address:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(58, 304);
+            label4.Name = "label4";
+            label4.Size = new Size(144, 22);
+            label4.TabIndex = 6;
+            label4.Text = "Contact Number:";
+            // 
+            // btnRegister
+            // 
+            btnRegister.Location = new Point(1010, 714);
+            btnRegister.Name = "btnRegister";
+            btnRegister.Size = new Size(250, 64);
+            btnRegister.TabIndex = 7;
+            btnRegister.Text = "Register";
+            btnRegister.UseVisualStyleBackColor = true;
+            // 
             // ucAddCustomers
             // 
-            AutoScaleDimensions = new SizeF(11F, 28F);
+            AutoScaleDimensions = new SizeF(11F, 22F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnRegister);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(label1);
-            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Margin = new Padding(4, 4, 4, 4);
+            Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(4, 3, 4, 3);
             Name = "ucAddCustomers";
-            Size = new Size(2040, 1113);
+            Size = new Size(1319, 837);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -57,5 +134,12 @@
         #endregion
 
         private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button btnRegister;
     }
 }
--- ucDashboard.Designer.cs
+++ ucDashboard.Designer.cs
@@ -29,26 +29,51 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            label2 = new Label();
+            panel1 = new Panel();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(35, 199);
+            label1.Font = new Font("Times New Roman", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(66, 41);
             label1.Name = "label1";
-            label1.Size = new Size(43, 19);
+            label1.Size = new Size(194, 42);
             label1.TabIndex = 0;
-            label1.Text = "Dash";
+            label1.Text = "Dashboard";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(38, 38);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 23);
+            label2.TabIndex = 1;
+            label2.Text = "Total Sales:";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(38, 99);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1231, 698);
+            panel1.TabIndex = 2;
             // 
             // ucDashboard
             // 
             AutoScaleDimensions = new SizeF(9F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
+            Controls.Add(panel1);
             Controls.Add(label1);
             Font = new Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Name = "ucDashboard";
-            Size = new Size(1484, 795);
+            Size = new Size(1319, 837);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -56,5 +81,7 @@
         #endregion
 
         private Label label1;
+        private Label label2;
+        private Panel panel1;
     }
 }
