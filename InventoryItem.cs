ab69a21855913023c1b3c7ba720d04f32610d6f7
Ruther Calubayan <rcalubayan@juntec.com.ph>
2025-07-16 15:33:48 +0800

    added a class file to retrieve data

---
+++ InventoryItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal_Inventory_for_Juntec
+{
+    public class InventoryItem
+    {
+        public string ItemId { get; set; }
+        public string ItemName { get; set; }
+        public string ItemType { get; set; }
+        public string ItemSize { get; set; }
+        public string ItemCost { get; set; }
+    }
+}
