c60ea2f7e4243d59976ada0c2ffeb76dbaca604c
Ruther Calubayan <rcalubayan@juntec.com.ph>
2025-07-21 11:35:17 +0800

    Added new cost for dynamic change of milimiter

--- AssigningCustomer.Designer.cs
+++ AssigningCustomer.Designer.cs
@@ -32,15 +32,39 @@
             label2 = new Label();
             label3 = new Label();
             label4 = new Label();
-            lblItemID = new Label();
-            lblItemName = new Label();
-            lblItemType = new Label();
-            lblItemCost = new Label();
+            lblPrecision = new Label();
+            lblMaterial = new Label();
+            lblType = new Label();
+            lblBase = new Label();
             label5 = new Label();
-            lblItemSize = new Label();
+            lblShape = new Label();
             comboBox1 = new ComboBox();
             label6 = new Label();
             btnDone = new Button();
+            label7 = new Label();
+            txtQuantity = new TextBox();
+            lblBaseValue = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            lblPressureMax = new Label();
+            lblPressureMin = new Label();
+            lblPressureRange = new Label();
+            label23 = new Label();
+            label24 = new Label();
+            txtNote = new TextBox();
+            lblRadius = new Label();
+            lblHeight = new Label();
+            txtDiameter = new TextBox();
+            txtLength = new TextBox();
+            label16 = new Label();
+            lblCost = new Label();
+            label8 = new Label();
+            txtNewCost = new TextBox();
             SuspendLayout();
             // 
             // label1
@@ -48,27 +72,27 @@
             label1.AutoSize = true;
             label1.Location = new Point(37, 65);
             label1.Name = "label1";
-            label1.Size = new Size(75, 22);
+            label1.Size = new Size(91, 22);
             label1.TabIndex = 0;
-            label1.Text = "Item ID:";
+            label1.Text = "Precision:";
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Location = new Point(37, 129);
             label2.Name = "label2";
-            label2.Size = new Size(101, 22);
+            label2.Size = new Size(84, 22);
             label2.TabIndex = 1;
-            label2.Text = "Item Name:";
+            label2.Text = "Material:";
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Location = new Point(37, 196);
             label3.Name = "label3";
-            label3.Size = new Size(94, 22);
+            label3.Size = new Size(55, 22);
             label3.TabIndex = 2;
-            label3.Text = "Item Type:";
+            label3.Text = "Type:";
             label3.Click += label3_Click;
             // 
             // label4
@@ -76,69 +100,69 @@
             label4.AutoSize = true;
             label4.Location = new Point(37, 338);
             label4.Name = "label4";
-            label4.Size = new Size(91, 22);
+            label4.Size = new Size(55, 22);
             label4.TabIndex = 3;
-            label4.Text = "Item Cost:";
+            label4.Text = "Base:";
             // 
-            // lblItemID
+            // lblPrecision
             // 
-            lblItemID.AutoSize = true;
-            lblItemID.Location = new Point(170, 65);
-            lblItemID.Name = "lblItemID";
-            lblItemID.Size = new Size(17, 22);
-            lblItemID.TabIndex = 4;
-            lblItemID.Text = "-";
+            lblPrecision.AutoSize = true;
+            lblPrecision.Location = new Point(170, 65);
+            lblPrecision.Name = "lblPrecision";
+            lblPrecision.Size = new Size(17, 22);
+            lblPrecision.TabIndex = 4;
+            lblPrecision.Text = "-";
             // 
-            // lblItemName
+            // lblMaterial
             // 
-            lblItemName.AutoSize = true;
-            lblItemName.Location = new Point(170, 129);
-            lblItemName.Name = "lblItemName";
-            lblItemName.Size = new Size(17, 22);
-            lblItemName.TabIndex = 5;
-            lblItemName.Text = "-";
+            lblMaterial.AutoSize = true;
+            lblMaterial.Location = new Point(170, 129);
+            lblMaterial.Name = "lblMaterial";
+            lblMaterial.Size = new Size(17, 22);
+            lblMaterial.TabIndex = 5;
+            lblMaterial.Text = "-";
             // 
-            // lblItemType
+            // lblType
             // 
-            lblItemType.AutoSize = true;
-            lblItemType.Location = new Point(170, 196);
-            lblItemType.Name = "lblItemType";
-            lblItemType.Size = new Size(17, 22);
-            lblItemType.TabIndex = 6;
-            lblItemType.Text = "-";
+            lblType.AutoSize = true;
+            lblType.Location = new Point(170, 196);
+            lblType.Name = "lblType";
+            lblType.Size = new Size(17, 22);
+            lblType.TabIndex = 6;
+            lblType.Text = "-";
             // 
-            // lblItemCost
+            // lblBase
             // 
-            lblItemCost.AutoSize = true;
-            lblItemCost.Location = new Point(170, 338);
-            lblItemCost.Name = "lblItemCost";
-            lblItemCost.Size = new Size(17, 22);
-            lblItemCost.TabIndex = 7;
-            lblItemCost.Text = "-";
+            lblBase.AutoSize = true;
+            lblBase.Location = new Point(170, 338);
+            lblBase.Name = "lblBase";
+            lblBase.Size = new Size(17, 22);
+            lblBase.TabIndex = 7;
+            lblBase.Text = "-";
             // 
             // label5
             // 
             label5.AutoSize = true;
             label5.Location = new Point(37, 266);
             label5.Name = "label5";
-            label5.Size = new Size(89, 22);
+            label5.Size = new Size(64, 22);
             label5.TabIndex = 8;
-            label5.Text = "Item Size:";
+            label5.Text = "Shape:";
             // 
-            // lblItemSize
+            // lblShape
             // 
-            lblItemSize.AutoSize = true;
-            lblItemSize.Location = new Point(170, 266);
-            lblItemSize.Name = "lblItemSize";
-            lblItemSize.Size = new Size(17, 22);
-            lblItemSize.TabIndex = 9;
-            lblItemSize.Text = "-";
+            lblShape.AutoSize = true;
+            lblShape.Location = new Point(170, 266);
+            lblShape.Name = "lblShape";
+            lblShape.Size = new Size(17, 22);
+            lblShape.TabIndex = 9;
+            lblShape.Text = "-";
             // 
             // comboBox1
             // 
             comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(170, 407);
+            comboBox1.Location = new Point(452, 522);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(297, 30);
             comboBox1.TabIndex = 10;
@@ -147,7 +171,7 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(37, 410);
+            label6.Location = new Point(320, 525);
             label6.Name = "label6";
             label6.Size = new Size(91, 22);
             label6.TabIndex = 11;
@@ -155,28 +179,261 @@
             // 
             // btnDone
             // 
-            btnDone.Location = new Point(197, 486);
+            btnDone.Location = new Point(871, 554);
             btnDone.Name = "btnDone";
-            btnDone.Size = new Size(146, 43);
+            btnDone.Size = new Size(202, 66);
             btnDone.TabIndex = 12;
             btnDone.Text = "Done";
             btnDone.UseVisualStyleBackColor = true;
             btnDone.Click += btnDone_Click;
             // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(339, 399);
+            label7.Name = "label7";
+            label7.Size = new Size(82, 22);
+            label7.TabIndex = 13;
+            label7.Text = "Quantity:";
+            // 
+            // txtQuantity
+            // 
+            txtQuantity.Location = new Point(464, 399);
+            txtQuantity.Name = "txtQuantity";
+            txtQuantity.Size = new Size(68, 30);
+            txtQuantity.TabIndex = 14;
+            txtQuantity.TextChanged += txtQuantity_TextChanged;
+            // 
+            // lblBaseValue
+            // 
+            lblBaseValue.AutoSize = true;
+            lblBaseValue.Location = new Point(170, 399);
+            lblBaseValue.Name = "lblBaseValue";
+            lblBaseValue.Size = new Size(17, 22);
+            lblBaseValue.TabIndex = 16;
+            lblBaseValue.Text = "-";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(37, 399);
+            label9.Name = "label9";
+            label9.Size = new Size(104, 22);
+            label9.TabIndex = 15;
+            label9.Text = "Base Value:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(37, 456);
+            label10.Name = "label10";
+            label10.Size = new Size(89, 22);
+            label10.TabIndex = 17;
+            label10.Text = "Diameter:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(37, 520);
+            label11.Name = "label11";
+            label11.Size = new Size(68, 22);
+            label11.TabIndex = 18;
+            label11.Text = "Length:";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(329, 65);
+            label12.Name = "label12";
+            label12.Size = new Size(138, 22);
+            label12.TabIndex = 19;
+            label12.Text = "Pressure Range:";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(329, 135);
+            label13.Name = "label13";
+            label13.Size = new Size(124, 22);
+            label13.TabIndex = 20;
+            label13.Text = "Pressure Max:";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(329, 207);
+            label14.Name = "label14";
+            label14.Size = new Size(121, 22);
+            label14.TabIndex = 21;
+            label14.Text = "Pressure Min:";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(350, 266);
+            label15.Name = "label15";
+            label15.Size = new Size(71, 22);
+            label15.TabIndex = 22;
+            label15.Text = "Radius:";
+            label15.Click += label15_Click;
+            // 
+            // lblPressureMax
+            // 
+            lblPressureMax.AutoSize = true;
+            lblPressureMax.Location = new Point(515, 135);
+            lblPressureMax.Name = "lblPressureMax";
+            lblPressureMax.Size = new Size(17, 22);
+            lblPressureMax.TabIndex = 27;
+            lblPressureMax.Text = "-";
+            // 
+            // lblPressureMin
+            // 
+            lblPressureMin.AutoSize = true;
+            lblPressureMin.Location = new Point(515, 207);
+            lblPressureMin.Name = "lblPressureMin";
+            lblPressureMin.Size = new Size(17, 22);
+            lblPressureMin.TabIndex = 26;
+            lblPressureMin.Text = "-";
+            // 
+            // lblPressureRange
+            // 
+            lblPressureRange.AutoSize = true;
+            lblPressureRange.Location = new Point(515, 65);
+            lblPressureRange.Name = "lblPressureRange";
+            lblPressureRange.Size = new Size(17, 22);
+            lblPressureRange.TabIndex = 25;
+            lblPressureRange.Text = "-";
+            // 
+            // label23
+            // 
+            label23.AutoSize = true;
+            label23.Location = new Point(353, 338);
+            label23.Name = "label23";
+            label23.Size = new Size(68, 22);
+            label23.TabIndex = 29;
+            label23.Text = "Height:";
+            // 
+            // label24
+            // 
+            label24.AutoSize = true;
+            label24.Location = new Point(691, 65);
+            label24.Name = "label24";
+            label24.Size = new Size(54, 22);
+            label24.TabIndex = 31;
+            label24.Text = "Note:";
+            // 
+            // txtNote
+            // 
+            txtNote.Location = new Point(691, 102);
+            txtNote.Multiline = true;
+            txtNote.Name = "txtNote";
+            txtNote.Size = new Size(447, 127);
+            txtNote.TabIndex = 32;
+            // 
+            // lblRadius
+            // 
+            lblRadius.AutoSize = true;
+            lblRadius.Location = new Point(515, 268);
+            lblRadius.Name = "lblRadius";
+            lblRadius.Size = new Size(17, 22);
+            lblRadius.TabIndex = 28;
+            lblRadius.Text = "-";
+            // 
+            // lblHeight
+            // 
+            lblHeight.AutoSize = true;
+            lblHeight.Location = new Point(515, 338);
+            lblHeight.Name = "lblHeight";
+            lblHeight.Size = new Size(17, 22);
+            lblHeight.TabIndex = 30;
+            lblHeight.Text = "-";
+            // 
+            // txtDiameter
+            // 
+            txtDiameter.Location = new Point(170, 453);
+            txtDiameter.Name = "txtDiameter";
+            txtDiameter.Size = new Size(68, 30);
+            txtDiameter.TabIndex = 33;
+            // 
+            // txtLength
+            // 
+            txtLength.Location = new Point(170, 517);
+            txtLength.Name = "txtLength";
+            txtLength.Size = new Size(68, 30);
+            txtLength.TabIndex = 34;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(327, 461);
+            label16.Name = "label16";
+            label16.Size = new Size(123, 22);
+            label16.TabIndex = 35;
+            label16.Text = "Original Cost:";
+            // 
+            // lblCost
+            // 
+            lblCost.AutoSize = true;
+            lblCost.Location = new Point(515, 461);
+            lblCost.Name = "lblCost";
+            lblCost.Size = new Size(17, 22);
+            lblCost.TabIndex = 36;
+            lblCost.Text = "-";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(691, 266);
+            label8.Name = "label8";
+            label8.Size = new Size(95, 22);
+            label8.TabIndex = 37;
+            label8.Text = "New Cost:";
+            // 
+            // txtNewCost
+            // 
+            txtNewCost.Location = new Point(827, 263);
+            txtNewCost.Name = "txtNewCost";
+            txtNewCost.Size = new Size(157, 30);
+            txtNewCost.TabIndex = 38;
+            // 
             // AssigningCustomer
             // 
             AutoScaleDimensions = new SizeF(11F, 22F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(559, 583);
+            ClientSize = new Size(1174, 657);
+            Controls.Add(txtNewCost);
+            Controls.Add(label8);
+            Controls.Add(lblCost);
+            Controls.Add(label16);
+            Controls.Add(txtLength);
+            Controls.Add(txtDiameter);
+            Controls.Add(txtNote);
+            Controls.Add(label24);
+            Controls.Add(lblHeight);
+            Controls.Add(label23);
+            Controls.Add(lblRadius);
+            Controls.Add(lblPressureMax);
+            Controls.Add(lblPressureMin);
+            Controls.Add(lblPressureRange);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(lblBaseValue);
+            Controls.Add(label9);
+            Controls.Add(txtQuantity);
+            Controls.Add(label7);
             Controls.Add(btnDone);
             Controls.Add(label6);
             Controls.Add(comboBox1);
-            Controls.Add(lblItemSize);
+            Controls.Add(lblShape);
             Controls.Add(label5);
-            Controls.Add(lblItemCost);
-            Controls.Add(lblItemType);
-            Controls.Add(lblItemName);
-            Controls.Add(lblItemID);
+            Controls.Add(lblBase);
+            Controls.Add(lblType);
+            Controls.Add(lblMaterial);
+            Controls.Add(lblPrecision);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
@@ -199,14 +456,38 @@
         private Label label2;
         private Label label3;
         private Label label4;
-        private Label lblItemID;
-        private Label lblItemName;
-        private Label lblItemType;
-        private Label lblItemCost;
+        private Label lblPrecision;
+        private Label lblMaterial;
+        private Label lblType;
+        private Label lblBase;
         private Label label5;
-        private Label lblItemSize;
+        private Label lblShape;
         private ComboBox comboBox1;
         private Label label6;
         private Button btnDone;
+        private Label label7;
+        private TextBox txtQuantity;
+        private Label lblBaseValue;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label lblPressureMax;
+        private Label lblPressureMin;
+        private Label lblPressureRange;
+        private Label label23;
+        private Label label24;
+        private TextBox txtNote;
+        private Label lblRadius;
+        private Label lblHeight;
+        private TextBox txtDiameter;
+        private TextBox txtLength;
+        private Label label16;
+        private Label lblCost;
+        private Label label8;
+        private TextBox txtNewCost;
     }
 }
--- AssigningCustomer.cs
+++ AssigningCustomer.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace Personal_Inventory_for_Juntec
 {
@@ -25,22 +26,33 @@ namespace Personal_Inventory_for_Juntec
 
         private void LoadItemData()
         {
-            lblItemID.Text = item.ItemId;
-            lblItemName.Text = item.ItemName;
-            lblItemType.Text = item.ItemType;
-            lblItemSize.Text = item.ItemSize;
-            lblItemCost.Text = item.ItemCost;
+            lblPrecision.Text = item.PrecisionLevel;
+            lblMaterial.Text = item.Material;
+            lblType.Text = item.Type;
+            lblShape.Text = item.ShapeCode.ToString();
+            lblBase.Text = item.BaseCode.ToString();
+            lblBaseValue.Text = item.BaseValue.ToString("0.00");
+            txtDiameter.Text = item.Diameter.ToString("0.00");
+            txtLength.Text = item.Length.ToString("0.00");
+            lblPressureRange.Text = string.IsNullOrEmpty(item.PressureRange) ? "—" : item.PressureRange;
+            lblPressureMax.Text = item.PressureMax.ToString("0.00");
+            lblPressureMin.Text = item.PressureMin.ToString("0.00");
+            lblRadius.Text = item.RadiusTolerance.ToString("0.000");
+            lblHeight.Text = item.Height.ToString("0.00");
+            lblCost.Text = item.Cost.ToString("C2", new CultureInfo("en-PH"));
         }
 
         private void AssigningCustomer_Load(object sender, EventArgs e)
         {
             LoadCustomers();
+            LoadItemData();
         }
 
         private void label3_Click(object sender, EventArgs e)
         {
 
         }
+
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -49,7 +61,7 @@ namespace Personal_Inventory_for_Juntec
 
         private void LoadCustomers()
         {
-            comboBox1.Items.Clear(); // Clear existing items
+            comboBox1.Items.Clear();
 
             using (SqlConnection conn = new SqlConnection("Data Source=RCALUBAYAN\\SQLEXPRESS;Initial Catalog=PersonalDB;Integrated Security=True;Trust Server Certificate=True"))
             {
@@ -77,6 +89,115 @@ namespace Personal_Inventory_for_Juntec
         }
 
         private void btnDone_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtQuantity.Text.Trim(), out int requestedQty) || requestedQty <= 0)
+            {
+                MessageBox.Show("Please enter a valid quantity.", "Validation Error");
+                return;
+            }
+
+            if (comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a customer.", "Validation Error");
+                return;
+            }
+
+            if (!decimal.TryParse(txtDiameter.Text.Trim(), out decimal updatedDiameter) ||
+                !decimal.TryParse(txtLength.Text.Trim(), out decimal updatedLength))
+            {
+                MessageBox.Show("Please enter valid numeric values for diameter and length.", "Validation Error");
+                return;
+            }
+
+            string[] names = comboBox1.SelectedItem.ToString().Split(' ');
+            if (names.Length < 2)
+            {
+                MessageBox.Show("Invalid customer name format.");
+                return;
+            }
+
+            string firstName = names[0];
+            string lastName = names[1];
+            int inventoryId = item.MaterialID;
+
+            using (SqlConnection conn = new SqlConnection("Data Source=RCALUBAYAN\\SQLEXPRESS;Initial Catalog=PersonalDB;Integrated Security=True;Trust Server Certificate=True"))
+            {
+                conn.Open();
+                SqlTransaction transaction = conn.BeginTransaction();
+                try
+                {
+                    SqlCommand getCustomerIdCmd = new SqlCommand("SELECT id FROM customer WHERE first_name = @first AND last_name = @last", conn, transaction);
+                    getCustomerIdCmd.Parameters.AddWithValue("@first", firstName);
+                    getCustomerIdCmd.Parameters.AddWithValue("@last", lastName);
+                    object result = getCustomerIdCmd.ExecuteScalar();
+
+                    if (result == null)
+                        throw new Exception("Customer not found.");
+
+                    int customerId = Convert.ToInt32(result);
+
+                    SqlCommand getStockCmd = new SqlCommand("SELECT quantity FROM inventory WHERE materialID = @id", conn, transaction);
+                    getStockCmd.Parameters.AddWithValue("@id", inventoryId);
+                    int currentStock = Convert.ToInt32(getStockCmd.ExecuteScalar());
+
+                    if (requestedQty > currentStock)
+                        throw new Exception("Not enough stock available.");
+
+                    SqlCommand updateStockCmd = new SqlCommand("UPDATE inventory SET quantity = quantity - @qty WHERE materialID = @id", conn, transaction);
+                    updateStockCmd.Parameters.AddWithValue("@qty", requestedQty);
+                    updateStockCmd.Parameters.AddWithValue("@id", inventoryId);
+                    updateStockCmd.ExecuteNonQuery();
+
+                    SqlCommand insertCmd = new SqlCommand(@"INSERT INTO customer_inventory_detailed ( customerName, assignedDate, assignedQuantity,
+                     precisionLevel, material, type, shapeCode, baseCode, baseValue,
+                     diameter, length, pressureRange, pressureMax, pressureMin,
+                     radiusTolerance, height, notes, inventoryID, @cost)
+                     VALUES (
+                      @customerName, GETDATE(), @assignedQuantity,
+                      @precisionLevel, @material, @type, @shapeCode, @baseCode, @baseValue,
+                      @diameter, @length, @pressureRange, @pressureMax, @pressureMin,
+                      @radiusTolerance, @height, @notes, @inventoryID, @cost)", conn, transaction);
+
+
+                    insertCmd.Parameters.AddWithValue("@customerName", comboBox1.SelectedItem.ToString());
+                    insertCmd.Parameters.AddWithValue("@assignedQuantity", requestedQty);
+                    insertCmd.Parameters.AddWithValue("@precisionLevel", item.PrecisionLevel);
+                    insertCmd.Parameters.AddWithValue("@material", item.Material);
+                    insertCmd.Parameters.AddWithValue("@type", item.Type);
+                    insertCmd.Parameters.AddWithValue("@shapeCode", item.ShapeCode);
+                    insertCmd.Parameters.AddWithValue("@baseCode", item.BaseCode);
+                    insertCmd.Parameters.AddWithValue("@baseValue", item.BaseValue);
+                    insertCmd.Parameters.AddWithValue("@diameter", updatedDiameter);
+                    insertCmd.Parameters.AddWithValue("@length", updatedLength);
+                    insertCmd.Parameters.AddWithValue("@pressureRange", item.PressureRange);
+                    insertCmd.Parameters.AddWithValue("@pressureMax", item.PressureMax);
+                    insertCmd.Parameters.AddWithValue("@pressureMin", item.PressureMin);
+                    insertCmd.Parameters.AddWithValue("@radiusTolerance", item.RadiusTolerance);
+                    insertCmd.Parameters.AddWithValue("@height", item.Height);
+                    insertCmd.Parameters.AddWithValue("@notes", txtNote.Text);
+                    insertCmd.Parameters.AddWithValue("@inventoryID", inventoryId);
+                    insertCmd.Parameters.AddWithValue("@cost", txtNewCost.Text);
+
+                    insertCmd.ExecuteNonQuery();
+                    transaction.Commit();
+
+                    MessageBox.Show("Item successfully set in Customer's Inventory.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    MessageBox.Show("Failed to assign item: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void txtQuantity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
         {
 
         }
