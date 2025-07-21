72ad4a11b79b2f5c1fe9518f0c03d53d240518f1
Ruther Calubayan <rcalubayan@juntec.com.ph>
2025-07-21 11:34:00 +0800

    added new features

--- ucViewInventory.Designer.cs
+++ ucViewInventory.Designer.cs
@@ -18,15 +18,27 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            txtSearch = new TextBox();
             label2 = new Label();
-            comboBox1 = new ComboBox();
             cbType = new ComboBox();
+            cbMaterial = new ComboBox();
             label3 = new Label();
             dtgInventory = new DataGridView();
             btnSearch = new Button();
             btnNext = new Button();
             lblLoading = new Label();
+            cbPrecision = new ComboBox();
+            label4 = new Label();
+            label5 = new Label();
+            cbShape = new ComboBox();
+            cbBase = new ComboBox();
+            label6 = new Label();
+            label7 = new Label();
+            cbBaseValue = new ComboBox();
+            label8 = new Label();
+            txtDiameter = new TextBox();
+            txtLength = new TextBox();
+            label9 = new Label();
+            btnClear = new Button();
             ((System.ComponentModel.ISupportInitialize)dtgInventory).BeginInit();
             SuspendLayout();
             // 
@@ -40,72 +52,66 @@
             label1.TabIndex = 0;
             label1.Text = "View Inventory";
             // 
-            // txtSearch
-            // 
-            txtSearch.Location = new Point(88, 109);
-            txtSearch.Name = "txtSearch";
-            txtSearch.Size = new Size(438, 30);
-            txtSearch.TabIndex = 1;
-            txtSearch.TextChanged += txtSearch_TextChanged;
-            // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(88, 75);
+            label2.Location = new Point(33, 84);
             label2.Name = "label2";
-            label2.Size = new Size(122, 22);
+            label2.Size = new Size(91, 22);
             label2.TabIndex = 2;
-            label2.Text = "Name of item:";
-            // 
-            // comboBox1
-            // 
-            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(977, 109);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(232, 30);
-            comboBox1.TabIndex = 3;
+            label2.Text = "Precision:";
             // 
             // cbType
             // 
             cbType.DropDownStyle = ComboBoxStyle.DropDownList;
             cbType.FormattingEnabled = true;
-            cbType.Location = new Point(712, 109);
+            cbType.Location = new Point(301, 118);
             cbType.Name = "cbType";
-            cbType.Size = new Size(232, 30);
-            cbType.TabIndex = 4;
-            cbType.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            cbType.Size = new Size(128, 30);
+            cbType.TabIndex = 3;
+            // 
+            // cbMaterial
+            // 
+            cbMaterial.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbMaterial.FormattingEnabled = true;
+            cbMaterial.Location = new Point(167, 118);
+            cbMaterial.Name = "cbMaterial";
+            cbMaterial.Size = new Size(128, 30);
+            cbMaterial.TabIndex = 4;
+            cbMaterial.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(712, 75);
+            label3.Location = new Point(167, 84);
             label3.Name = "label3";
-            label3.Size = new Size(55, 22);
+            label3.Size = new Size(84, 22);
             label3.TabIndex = 5;
-            label3.Text = "Type:";
+            label3.Text = "Material:";
             // 
             // dtgInventory
             // 
-            dtgInventory.BackgroundColor = SystemColors.ActiveCaption;
+            dtgInventory.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            dtgInventory.BackgroundColor = Color.White;
             dtgInventory.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dtgInventory.Location = new Point(88, 177);
+            dtgInventory.Location = new Point(22, 181);
             dtgInventory.Name = "dtgInventory";
             dtgInventory.RowHeadersWidth = 51;
-            dtgInventory.Size = new Size(1121, 551);
+            dtgInventory.Size = new Size(1263, 547);
             dtgInventory.TabIndex = 6;
             dtgInventory.CellContentClick += dtgInventory_CellContentClick;
             // 
             // btnSearch
             // 
+            btnSearch.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             btnSearch.BackColor = SystemColors.GrayText;
             btnSearch.FlatAppearance.BorderSize = 0;
             btnSearch.FlatAppearance.MouseOverBackColor = Color.Silver;
             btnSearch.FlatStyle = FlatStyle.Flat;
             btnSearch.ForeColor = Color.White;
-            btnSearch.Location = new Point(543, 108);
+            btnSearch.Location = new Point(1048, 97);
             btnSearch.Name = "btnSearch";
-            btnSearch.Size = new Size(110, 30);
+            btnSearch.Size = new Size(118, 38);
             btnSearch.TabIndex = 7;
             btnSearch.Text = "Search";
             btnSearch.UseVisualStyleBackColor = false;
@@ -113,9 +119,10 @@
             // 
             // btnNext
             // 
-            btnNext.Location = new Point(1074, 769);
+            btnNext.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnNext.Location = new Point(1099, 761);
             btnNext.Name = "btnNext";
-            btnNext.Size = new Size(135, 38);
+            btnNext.Size = new Size(165, 53);
             btnNext.TabIndex = 8;
             btnNext.Text = "Next";
             btnNext.UseVisualStyleBackColor = true;
@@ -132,19 +139,160 @@
             lblLoading.Text = "Loading...";
             lblLoading.Visible = false;
             // 
+            // cbPrecision
+            // 
+            cbPrecision.BackColor = Color.White;
+            cbPrecision.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbPrecision.FormattingEnabled = true;
+            cbPrecision.Location = new Point(33, 118);
+            cbPrecision.Name = "cbPrecision";
+            cbPrecision.Size = new Size(128, 30);
+            cbPrecision.TabIndex = 10;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(301, 84);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 22);
+            label4.TabIndex = 11;
+            label4.Text = "Type:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(435, 84);
+            label5.Name = "label5";
+            label5.Size = new Size(64, 22);
+            label5.TabIndex = 12;
+            label5.Text = "Shape:";
+            // 
+            // cbShape
+            // 
+            cbShape.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbShape.FormattingEnabled = true;
+            cbShape.Location = new Point(435, 118);
+            cbShape.Name = "cbShape";
+            cbShape.Size = new Size(128, 30);
+            cbShape.TabIndex = 13;
+            // 
+            // cbBase
+            // 
+            cbBase.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbBase.FormattingEnabled = true;
+            cbBase.Location = new Point(569, 118);
+            cbBase.Name = "cbBase";
+            cbBase.Size = new Size(128, 30);
+            cbBase.TabIndex = 15;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(569, 81);
+            label6.Name = "label6";
+            label6.Size = new Size(99, 22);
+            label6.TabIndex = 14;
+            label6.Text = "Base Type:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(703, 84);
+            label7.Name = "label7";
+            label7.Size = new Size(104, 22);
+            label7.TabIndex = 16;
+            label7.Text = "Base Value:";
+            // 
+            // cbBaseValue
+            // 
+            cbBaseValue.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbBaseValue.FormattingEnabled = true;
+            cbBaseValue.Location = new Point(703, 118);
+            cbBaseValue.Name = "cbBaseValue";
+            cbBaseValue.Size = new Size(128, 30);
+            cbBaseValue.TabIndex = 17;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(847, 81);
+            label8.Name = "label8";
+            label8.Size = new Size(89, 22);
+            label8.TabIndex = 18;
+            label8.Text = "Diameter:";
+            label8.Click += label8_Click;
+            // 
+            // txtDiameter
+            // 
+            txtDiameter.BackColor = Color.Gray;
+            txtDiameter.ForeColor = Color.White;
+            txtDiameter.Location = new Point(847, 118);
+            txtDiameter.Name = "txtDiameter";
+            txtDiameter.Size = new Size(79, 30);
+            txtDiameter.TabIndex = 19;
+            txtDiameter.TextChanged += textBox1_TextChanged;
+            // 
+            // txtLength
+            // 
+            txtLength.BackColor = Color.Gray;
+            txtLength.ForeColor = Color.White;
+            txtLength.Location = new Point(946, 118);
+            txtLength.Name = "txtLength";
+            txtLength.Size = new Size(79, 30);
+            txtLength.TabIndex = 21;
+            txtLength.TextChanged += textBox2_TextChanged;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(946, 81);
+            label9.Name = "label9";
+            label9.Size = new Size(68, 22);
+            label9.TabIndex = 20;
+            label9.Text = "Length:";
+            label9.Click += label9_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Anchor = AnchorStyles.Right;
+            btnClear.BackColor = SystemColors.GrayText;
+            btnClear.FlatAppearance.BorderSize = 0;
+            btnClear.FlatAppearance.MouseOverBackColor = Color.Silver;
+            btnClear.FlatStyle = FlatStyle.Flat;
+            btnClear.ForeColor = Color.White;
+            btnClear.Location = new Point(1177, 97);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(118, 38);
+            btnClear.TabIndex = 22;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = false;
+            btnClear.Click += button1_Click;
+            // 
             // ucViewInventory
             // 
             AutoScaleDimensions = new SizeF(11F, 22F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnClear);
+            Controls.Add(txtLength);
+            Controls.Add(label9);
+            Controls.Add(txtDiameter);
+            Controls.Add(label8);
+            Controls.Add(cbBaseValue);
+            Controls.Add(label7);
+            Controls.Add(cbBase);
+            Controls.Add(label6);
+            Controls.Add(cbShape);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(cbPrecision);
             Controls.Add(lblLoading);
             Controls.Add(btnNext);
             Controls.Add(btnSearch);
             Controls.Add(dtgInventory);
             Controls.Add(label3);
+            Controls.Add(cbMaterial);
             Controls.Add(cbType);
-            Controls.Add(comboBox1);
             Controls.Add(label2);
-            Controls.Add(txtSearch);
             Controls.Add(label1);
             Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Margin = new Padding(4, 3, 4, 3);
@@ -159,14 +307,26 @@
         #endregion
 
         private Label label1;
-        private TextBox txtSearch;
         private Label label2;
-        private ComboBox comboBox1;
         private ComboBox cbType;
+        private ComboBox cbMaterial;
         private Label label3;
         private DataGridView dtgInventory;
         private Button btnSearch;
         private Button btnNext;
         private Label lblLoading; // ← Added here
+        private ComboBox cbPrecision;
+        private Label label4;
+        private Label label5;
+        private ComboBox cbShape;
+        private ComboBox cbBase;
+        private Label label6;
+        private Label label7;
+        private ComboBox cbBaseValue;
+        private Label label8;
+        private TextBox txtDiameter;
+        private TextBox txtLength;
+        private Label label9;
+        private Button btnClear;
     }
 }
--- ucViewInventory.cs
+++ ucViewInventory.cs
@@ -23,16 +23,21 @@ namespace Personal_Inventory_for_Juntec
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //ApplyFilter();
+
         }
 
-        private async void ucViewInventory_Load(object sender, EventArgs e)
+        private void ucViewInventory_Load(object sender, EventArgs e)
         {
-            await LoadInventoryAsync();
-            LoadItemTypes();
+            LoadInventory();
+            LoadFilterValues("precisionLevel", cbPrecision);
+            LoadFilterValues("material", cbMaterial);
+            LoadFilterValues("type", cbType);
+            LoadFilterValues("shapeCode", cbShape);
+            LoadFilterValues("baseCode", cbBase);
+            LoadFilterValues("baseValue", cbBaseValue);
         }
 
-        private async Task LoadInventoryAsync()
+        private void LoadInventory()
         {
             lblLoading.Visible = true;
 
@@ -40,21 +45,38 @@ namespace Personal_Inventory_for_Juntec
             {
                 using (SqlConnection conn = new SqlConnection("Data Source=RCALUBAYAN\\SQLEXPRESS;Initial Catalog=PersonalDB;Integrated Security=True;Trust Server Certificate=True"))
                 {
-                    await conn.OpenAsync(); // Open connection asynchronously
+                    conn.Open();
 
-                    string query = "SELECT * FROM inventory";
+                    string query = "SELECT materialID, precisionLevel, material, type, shapeCode, baseCode, baseValue, diameter, length, pressureMax, pressureMin, radiusTolerance, height, quantity, cost FROM inventory";
                     using (SqlCommand cmd = new SqlCommand(query, conn))
-                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    using (SqlDataReader reader = cmd.ExecuteReader())
                     {
                         DataTable dt = new DataTable();
-                        dt.Load(reader); // Load data into DataTable
+                        dt.Load(reader);
                         inventoryTable = dt;
 
-                        dtgInventory.Invoke(() =>
-                        {
-                            dtgInventory.DataSource = inventoryTable;
-                            dtgInventory.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-                        });
+                        dtgInventory.DataSource = inventoryTable;
+                        dtgInventory.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+                        /*dtgInventory.Columns["pressureRange"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                        dtgInventory.Columns["pressureRange"].MinimumWidth = 100;*/
+
+                        dtgInventory.Columns["materialID"].HeaderText = "ID";
+                        dtgInventory.Columns["precisionLevel"].HeaderText = "Precision";
+                        dtgInventory.Columns["material"].HeaderText = "Material";
+                        dtgInventory.Columns["type"].HeaderText = "Type";
+                        dtgInventory.Columns["shapeCode"].HeaderText = "Shape";
+                        dtgInventory.Columns["baseCode"].HeaderText = "B";
+                        dtgInventory.Columns["baseValue"].HeaderText = "Bvalue";
+                        dtgInventory.Columns["diameter"].HeaderText = "D (mm)";
+                        dtgInventory.Columns["length"].HeaderText = "L (mm)";
+                        // dtgInventory.Columns["pressureRange"].HeaderText = "P Range";
+                        dtgInventory.Columns["pressureMax"].HeaderText = "P·Max";
+                        dtgInventory.Columns["pressureMin"].HeaderText = "P·Min";
+                        dtgInventory.Columns["radiusTolerance"].HeaderText = "R";
+                        dtgInventory.Columns["height"].HeaderText = "H";
+                        dtgInventory.Columns["quantity"].HeaderText = "Qty";
+                        dtgInventory.Columns["cost"].HeaderText = "Cost";
                     }
                 }
             }
@@ -68,59 +90,87 @@ namespace Personal_Inventory_for_Juntec
             }
         }
 
-        private void LoadItemTypes()
+        private void LoadFilterValues(string columnName, ComboBox comboBox)
         {
-            cbType.Items.Clear();
-            cbType.Items.Add(""); // Optional: empty item for "All"
+            comboBox.Items.Clear();
+            comboBox.Items.Add("");
 
+            string query = $"SELECT DISTINCT {columnName} FROM inventory WHERE {columnName} IS NOT NULL";
 
             using (SqlConnection conn = new SqlConnection("Data Source=RCALUBAYAN\\SQLEXPRESS;Initial Catalog=PersonalDB;Integrated Security=True;Trust Server Certificate=True"))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
             {
-                string query = "SELECT DISTINCT item_type FROM inventory WHERE item_type IS NOT NULL";
-                SqlCommand cmd = new SqlCommand(query, conn);
-
                 try
                 {
                     conn.Open();
                     SqlDataReader reader = cmd.ExecuteReader();
                     while (reader.Read())
                     {
-                        cbType.Items.Add(reader["item_type"].ToString());
+                        comboBox.Items.Add(reader[0].ToString());
                     }
-                    reader.Close();
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show("Failed to load types: " + ex.Message);
+                    MessageBox.Show($"Failed to load values for {columnName}: " + ex.Message);
                 }
             }
         }
 
-        private void ApplyFilter()
+        private void ApplyUniversalFilter()
         {
-            string search = txtSearch.Text.Trim().ToLower().Replace("'", "''");
-            string type = cbType.Text.Trim().ToLower().Replace("'", "''");
+            var view = (dtgInventory.DataSource as DataTable)?.DefaultView;
+            if (view == null) return;
 
-            // Start filter with search
-            string filter = $"item_name LIKE '%{search}%'";
+            List<string> conditions = new List<string>();
 
-            // Add type filter if selected
-            if (!string.IsNullOrWhiteSpace(type))
-            {
-                filter += $" AND item_type = '{type}'";
-            }
+            if (!string.IsNullOrWhiteSpace(cbPrecision.Text) && cbPrecision.Text != "All")
+                conditions.Add($"precisionLevel LIKE '%{cbPrecision.Text.Replace("'", "''")}%'");
 
-            (dtgInventory.DataSource as DataTable).DefaultView.RowFilter = filter;
+            if (!string.IsNullOrWhiteSpace(cbMaterial.Text) && cbMaterial.Text != "All")
+                conditions.Add($"material LIKE '%{cbMaterial.Text.Replace("'", "''")}%'");
+
+            if (!string.IsNullOrWhiteSpace(cbType.Text) && cbType.Text != "All")
+                conditions.Add($"type LIKE '%{cbType.Text.Replace("'", "''")}%'");
+
+            if (!string.IsNullOrWhiteSpace(cbShape.Text) && cbShape.Text != "All")
+                conditions.Add($"shapeCode LIKE '%{cbShape.Text.Replace("'", "''")}%'");
+
+            if (!string.IsNullOrWhiteSpace(cbBase.Text) && cbBase.Text != "All")
+                conditions.Add($"baseCode LIKE '%{cbBase.Text.Replace("'", "''")}%'");
+
+            if (!string.IsNullOrWhiteSpace(cbBaseValue.Text) && cbBaseValue.Text != "All")
+                conditions.Add($"CONVERT(baseValue, 'System.String') LIKE '%{cbBaseValue.Text.Replace("'", "''")}%'");
+
+            if (decimal.TryParse(txtDiameter.Text.Trim(), out decimal diameterValue))
+                conditions.Add($"diameter = {diameterValue}");
+
+
+            if (decimal.TryParse(txtLength.Text.Trim(), out decimal lengthValue))
+                conditions.Add($"length = {lengthValue}");
+
+
+            string filter = string.Join(" AND ", conditions);
+            view.RowFilter = filter;
+
+            var filteredTable = view.ToTable();
         }
+
+
+
 
         private void txtSearch_TextChanged(object sender, EventArgs e)
         {
-            //ApplyFilter();
+
         }
 
         private void dtgInventory_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             btnNext.Enabled = dtgInventory.CurrentRow != null && !dtgInventory.CurrentRow.IsNewRow;
+        }
+
+        private decimal GetDecimalValue(object value)
+        {
+            return value != null ? Convert.ToDecimal(value) : 0.00M;
         }
 
         private void btnNext_Click(object sender, EventArgs e)
@@ -131,11 +181,22 @@ namespace Personal_Inventory_for_Juntec
                 {
                     var item = new InventoryItem
                     {
-                        ItemId = dtgInventory.CurrentRow.Cells["id"].Value?.ToString(),
-                        ItemName = dtgInventory.CurrentRow.Cells["item_name"].Value?.ToString(),
-                        ItemType = dtgInventory.CurrentRow.Cells["item_type"].Value?.ToString(),
-                        ItemSize = dtgInventory.CurrentRow.Cells["item_size"].Value?.ToString(),
-                        ItemCost = dtgInventory.CurrentRow.Cells["item_cost"].Value?.ToString()
+                        MaterialID = Convert.ToInt32(dtgInventory.CurrentRow.Cells["materialID"].Value),
+                        PrecisionLevel = dtgInventory.CurrentRow.Cells["precisionLevel"].Value?.ToString(),
+                        Material = dtgInventory.CurrentRow.Cells["material"].Value?.ToString(),
+                        Type = dtgInventory.CurrentRow.Cells["type"].Value?.ToString(),
+                        ShapeCode = Convert.ToChar(dtgInventory.CurrentRow.Cells["shapeCode"].Value),
+                        BaseCode = Convert.ToChar(dtgInventory.CurrentRow.Cells["baseCode"].Value),
+                        BaseValue = GetDecimalValue(dtgInventory.CurrentRow.Cells["baseValue"].Value),
+                        Diameter = GetDecimalValue(dtgInventory.CurrentRow.Cells["diameter"].Value),
+                        Length = GetDecimalValue(dtgInventory.CurrentRow.Cells["length"].Value),
+                        PressureRange = dtgInventory.CurrentRow.Cells["pressureRange"].Value?.ToString() ?? "—",
+                        PressureMax = GetDecimalValue(dtgInventory.CurrentRow.Cells["pressureMax"].Value),
+                        PressureMin = GetDecimalValue(dtgInventory.CurrentRow.Cells["pressureMin"].Value),
+                        RadiusTolerance = GetDecimalValue(dtgInventory.CurrentRow.Cells["radiusTolerance"].Value),
+                        Height = GetDecimalValue(dtgInventory.CurrentRow.Cells["height"].Value),
+                        Quantity = Convert.ToInt32(dtgInventory.CurrentRow.Cells["quantity"].Value),
+                        Cost = GetDecimalValue(dtgInventory.CurrentRow.Cells["cost"].Value)
                     };
 
                     AssigningCustomer assignForm = new AssigningCustomer(item);
@@ -154,7 +215,47 @@ namespace Personal_Inventory_for_Juntec
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
-            ApplyFilter();
+            ApplyUniversalFilter();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cbPrecision.SelectedIndex = -1;
+            cbMaterial.SelectedIndex = -1;
+            cbType.SelectedIndex = -1;
+            cbShape.SelectedIndex = -1;
+            cbBase.SelectedIndex = -1;
+            cbBaseValue.SelectedIndex = -1;
+
+            txtDiameter.Text = "";
+            txtLength.Text = "";
+
+            // 🗑 Clear DataGridView filter and show all rows
+            var view = (inventoryTable as DataTable)?.DefaultView;
+            if (view != null)
+            {
+                view.RowFilter = ""; // ✅ removes filtering
+                dtgInventory.DataSource = view.ToTable(); // refresh the grid
+            }
         }
     }
 }
